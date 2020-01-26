968fbd208dd6ab58a19b951e7a0e340287b60837
Varun Reddy <32846217+varunreddy98@users.noreply.github.com>
2020-01-26 16:49:12 +0530

    hibernate

---
+++ Hibernate/SampleWebApp/src/main/java/com/sample/webproject/entity/Course.java
@@ -0,0 +1,39 @@
+package com.sample.webproject.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+	
+	public Course()
+	{
+		
+	}
+	
+	public Course(String name)
+	{ 
+		this.name=name;
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	String name; 
+
+}
